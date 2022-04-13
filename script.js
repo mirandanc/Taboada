d89b8c5a7add55c99d10ce7aa2bcdaa49c9cdc76
mirandanc <miranda_chaves@hotmail.com>
2022-04-13 19:11:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0){
+        window.alert('Insira um numero para continuar')
+        } else { 
+        let n = Number(num.value)
+        let i = 1
+        tab.innerHTML = ''
+        while (i <= 10) { 
+            let item = document.createElement('option')
+            item.text = `${n} x ${i} = ${n*i}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            i++
+        } 
+     window.alert 
+    }
+}
